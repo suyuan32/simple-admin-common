b0778f2eb896b18babfc8cc9cb64415003b13206
Ryan Su <yuansu.china.work@gmail.com>
2023-05-12 11:07:48 +0800

    feat: add environment support for config

--- utils/captcha/conf.go
+++ utils/captcha/conf.go
@@ -2,8 +2,8 @@ package captcha
 
 // Conf is the captcha configuration structure
 type Conf struct {
-	KeyLong   int    `json:",optional,default=5"`                                 // captcha length
-	ImgWidth  int    `json:",optional,default=240"`                               // captcha width
-	ImgHeight int    `json:",optional,default=80"`                                // captcha height
-	Driver    string `json:",optional,default=digit,options=[digit,string,math]"` // captcha type
+	KeyLong   int    `json:",optional,default=5,env=CAPTCHA_KEY_LONG"`                               // captcha length
+	ImgWidth  int    `json:",optional,default=240,env=CAPTCHA_IMG_WIDTH"`                            // captcha width
+	ImgHeight int    `json:",optional,default=80,env=CAPTCHA_IMG_HEIGHT"`                            // captcha height
+	Driver    string `json:",optional,default=digit,options=[digit,string,math],env=CAPTCHA_DRIVER"` // captcha type
 }

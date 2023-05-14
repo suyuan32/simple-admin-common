688bb29aee83498fb4e7f06f90798013b02030df
Ryan Su <yuansu.china.work@gmail.com>
2023-05-14 11:17:26 +0800

    fix: optimize error msg

--- msg/errormsg/msg.go
+++ msg/errormsg/msg.go
@@ -1,5 +1,12 @@
 package errormsg
 
 const (
-	SUCCESS string = "successful"
+	Success       string = "successful"
+	Fail          string = "failed"
+	CreateSuccess string = "create successfully"
+	CreateFail    string = "create failed"
+	UpdateSuccess string = "update successfully"
+	UpdateFail    string = "update failed"
+	DeleteSuccess string = "delete successfully"
+	DeleteFail    string = "delete successfully"
 )

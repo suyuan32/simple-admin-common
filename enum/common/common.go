eb0c2d9081e7b18ef7c3134622272da4daef7f78
Ryan SU <yuansu.china.work@gmail.com>
2023-07-03 08:49:30 +0800

    feat: DefaultInvalidRoleId

--- enum/common/common.go
+++ enum/common/common.go
@@ -7,4 +7,6 @@ const (
 
 const (
 	DefaultParentId uint64 = 1000000
+	// DefaultInvalidRoleId used to judge whether the token belongs to core, if it is DefaultInvalidRoleId, it belongs to mms
+	DefaultInvalidRoleId uint64 = 1000000
 )

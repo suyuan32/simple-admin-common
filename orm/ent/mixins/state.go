4a3679b29bd572e47caa3665eea08e0c8c1bbdae
Ryan SU <yuansu.china.work@gmail.com>
2023-08-04 08:06:00 +0800

    feat: ent state mixin

---
+++ orm/ent/mixins/state.go
@@ -0,0 +1,38 @@
+// Copyright 2023 The Ryan SU Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mixins
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// StateMixin implements the ent.Mixin for sharing
+// state fields with package schemas.
+type StateMixin struct {
+	mixin.Schema
+}
+
+func (StateMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Bool("state").
+			Default(true).
+			Optional().
+			Comment("state true: normal false ban | 状态 true 正常 false 禁用").
+			Annotations(entsql.WithComments(true)),
+	}
+}

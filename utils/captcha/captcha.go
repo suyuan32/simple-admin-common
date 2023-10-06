17870c286d128ea32fceb1f0ab68978c25b50681
Ryan Su <yuansu.china.work@gmail.com>
2023-10-06 08:06:42 +0800

    fix: chinese convert font in captcha

--- utils/captcha/captcha.go
+++ utils/captcha/captcha.go
@@ -126,6 +126,7 @@ func NewDriver(c Conf) base64Captcha.Driver {
 	case "chinese":
 		driver = base64Captcha.NewDriverChinese(c.ImgHeight, c.ImgWidth, 1, 1, c.KeyLong,
 			"天地玄黄宇宙洪荒日月盈辰宿列张寒来暑往秋收冬藏闰余成岁律吕调阳夫大水浸灌草木破坡石右云虫师军舰流浪数据速度", bgColor, nil, fonts)
+		driver = driver.(*base64Captcha.DriverChinese).ConvertFonts()
 	default:
 		driver = base64Captcha.NewDriverDigit(c.ImgHeight, c.ImgWidth,
 			c.KeyLong, 0.7, 80)

6b7b3ecb9d36a62bc3709874e0c3da8c2660fe20
Ryan Su <yuansu.china.work@gmail.com>
2024-07-04 07:53:04 +0800

    feat: ent string id mixin

---
+++ orm/ent/mixins/id_string.go
@@ -0,0 +1,43 @@
+// Copyright 2023 The Ryan SU Authors (https://github.com/suyuan32). All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mixins
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// IDStringMixin is the mixin with string type ID field
+// and the created_at, updated_at fields.
+type IDStringMixin struct {
+	mixin.Schema
+}
+
+func (IDStringMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id"),
+		field.Time("created_at").
+			Immutable().
+			Default(time.Now).
+			Comment("Create Time | 创建日期"),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now).
+			Comment("Update Time | 修改日期"),
+	}
+}

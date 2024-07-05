feffd25b2b7abc695a075d5cd653ed5e6de57eda
Ryan Su <yuansu.china.work@gmail.com>
2024-07-05 18:22:22 +0800

    fix: optimize data perm order

--- orm/ent/entenum/enum.go
+++ orm/ent/entenum/enum.go
@@ -9,14 +9,14 @@ const (
 	// DataPermAll is the data permission of all data
 	DataPermAll = 1
 
-	// DataPermOwnDept is the data permission of users's own department data
-	DataPermOwnDept = 2
+	// DataPermCustomDept is the data permission of custom department data
+	DataPermCustomDept = 2
 
 	// DataPermOwnDeptAndSub is the data permission of users's own department and sub departments data
 	DataPermOwnDeptAndSub = 3
 
-	// DataPermCustomDept is the data permission of custom department data
-	DataPermCustomDept = 4
+	// DataPermOwnDept is the data permission of users's own department data
+	DataPermOwnDept = 4
 
 	// DataPermSelf is the data permission of your own data
 	DataPermSelf = 5

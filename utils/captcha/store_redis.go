17e8f4e80ed01a7e5e30f8afe8ec045b984244fe
Ryan Su <yuansu.china.work@gmail.com>
2024-07-25 08:16:50 +0800

    doc: add comments

--- utils/captcha/store_redis.go
+++ utils/captcha/store_redis.go
@@ -1,12 +1,27 @@
+// Copyright 2023 The Ryan SU Authors (https://github.com/suyuan32). All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package captcha
 
 import (
 	"context"
+	"time"
+
 	"github.com/mojocn/base64Captcha"
 	"github.com/suyuan32/simple-admin-common/config"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
-	"time"
 )
 
 // NewRedisStore returns a redis store for captcha.
